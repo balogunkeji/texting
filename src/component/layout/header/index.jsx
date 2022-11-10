fd872f8e9f346ba55454b485d76d3a13fc87c073
keji <tomitoyo.93@gmail.com>
2022-11-10 19:04:55 +0100

    header

--- src/component/layout/header/index.jsx
+++ src/component/layout/header/index.jsx
@@ -1,9 +1,25 @@
 import React from 'react'
+import { Logo } from '../../../image';
 import './style.css'
  function Header() {
   return (
     <div className='header'>
-    Header
+      <div className="nav">
+      <div>
+        <img src={Logo} alt='logo'/>
+      </div>
+      <div className='ul'>
+      <p>Features</p>
+      <p>industries</p>
+      <p>Pricing</p>
+      <p>Solutions</p>
+      <p>Resources</p>
+      </div>
+      <div>
+        <button>Sign In</button>
+        <button>Start Free</button>
+      </div>
+      </div>
     </div>
   )
 }
